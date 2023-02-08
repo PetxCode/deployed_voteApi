a6443cb0c0a480f5e57721347c17ebb61c942449
PetxCode <brighterdayscodelab@gmail.com>
2023-02-08 02:43:13 +0100

    votersDB

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const url = "mongodb://localhost/votersDB";
 const newURL =
   "mongodb+srv://AuthClass:AuthClass@codelab.u4drr.mongodb.net/VotersDB?retryWrites=true&w=majority";
 const urlOnline =
-  "mongodb+srv://OneChurch:OneChurch@cluster0.q3zol.mongodb.net/youthCouncil?retryWrites=true&w=majority";
+  "mongodb+srv://OneChurch:OneChurch@cluster0.q3zol.mongodb.net/voterDB?retryWrites=true&w=majority";
 
 const url2 =
   "mongodb+srv://newStudent:newStudent@cluster0.gkpjkup.mongodb.net/voterDB?retryWrites=true&w=majority";
